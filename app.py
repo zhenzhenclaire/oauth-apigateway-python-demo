09d8fd033afd61d72ddcc85bada1c1afddb5feff
claire <clarezzhang@tencent.com>
2021-03-18 18:19:08 +0800

    commit

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
 from datetime import datetime
 from flask import request
 import python_jwt as jwt, jwcrypto.jwk as jwk, datetime
-from flask import Flask
+from wtforms import TextField, validators, SubmitField
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'C2HWGVoMGfNTBsrYQg8EcMrdTimkZfAb'
+Bootstrap(app)
 
+class LoginForm(FlaskForm):
+    username = TextField('Username:', validators=[validators.required()])
+    password = TextField('Password:', validators=[validators.required()])
+    submit = SubmitField('Submit')
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'POST':
+
+        username = form.username.data
+        password = form.password.data
+        if username == 'apigw' and password == 'apigwpsw':
+            code = "apigwcode"
+            return redirect(url_for("generate", code=code), 302)
+        else:
+            return render_template('error.html')
+    return render_template('form.html', form=form)
 
 
 def get_file_content(file):
@@ -38,18 +58,32 @@ def check():
 
 
 @app.route('/generate')
-def hello():
-    priv_pem = str(get_file_content('priv_pem')).encode('utf-8')
-    payload = {'foo': 'bar', 'wup': 90}
-    priv_key = jwk.JWK.from_pem(priv_pem)
-    token = jwt.generate_jwt(claims=payload, priv_key=priv_key, algorithm='RS256',
-                             lifetime=datetime.timedelta(minutes=5))
-    return token
+def generate():
+    auth_code = request.args.get("code")
+    print(auth_code)
+    auth_code_lib = ["apigwcode", "serverlesscode"]
+    if auth_code in auth_code_lib:
+        priv_pem = str(get_file_content('priv_pem')).encode('utf-8')
+        payload = {'foo': 'bar', 'wup': 90}
+        priv_key = jwk.JWK.from_pem(priv_pem)
+        token = jwt.generate_jwt(claims=payload, priv_key=priv_key, algorithm='RS256',
+                                 lifetime=datetime.timedelta(minutes=5))
+        return token
+    else:
+        return "invalid"
 
-# @route('/code')
-# def code():
-#     # todo   produce the code
-#     return "123456"
+
+@app.route('/code')
+def code():
+    username = request.args.get("username")
+    password = request.args.get("password")
+
+    if username == 'apigw' and password == 'apigwpsw':
+        return "apigwcode"
+    else:
+        return "-1"
+
+
 
 if __name__ == '__main__':
     app.run()
